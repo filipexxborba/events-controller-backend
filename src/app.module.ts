035264d6887f24b0c27d5f700d82b4329d564e20
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2023-05-04 09:37:55 -0300

    feat: implementing connection to the db by .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EventModule } from './event/event.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.gjqgh.mongodb.net/?retryWrites=true&w=majority`,
+    ),
     EventModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/events-controller'),
   ],
   controllers: [],
   providers: [],
